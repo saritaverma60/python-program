50822e37f692c310a9217bb60b93c034c65977e7
saritaverma60 <66161215+saritaverma60@users.noreply.github.com>
2020-05-30 14:18:19 +0530

    first p

---
+++ A1.py
@@ -0,0 +1,13 @@
+#A.py
+from math import pi
+def areac(r):
+    print( pi*r*r)
+    print("area: "+__name__)
+def arear(l,b):
+    print("area :", l*b)
+
+def main():   
+    (areac(4))
+    (arear(4,5))
+#if __name__=="__main__":
+main()
